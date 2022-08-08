82205cb041186e72bf971bc75f0cdc636b9ddd11
Jacqline <jacqlinegeng@gmail.com>
2022-08-08 18:00:19 -0400

    send HTML

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -15,6 +16,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
